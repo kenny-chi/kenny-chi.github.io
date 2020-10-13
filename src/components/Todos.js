9a4d9b5ca825f1e71f655f60857eb157ce918ac7
Kenny Chi <kennychi@berkeley.edu>
2020-10-13 03:03:22 -0700

    create react app

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render()  {
+        console.log(this.state)
+        return  (
+            <div>
+                <h1>Todos</h1>
+            </div>
+        )
+    }
+}
+
+export default Todos;
